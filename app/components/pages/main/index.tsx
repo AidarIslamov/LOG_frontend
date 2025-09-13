cec7514215e80a0e58630983270ee7b6e2e0f774
aidar <aidar.islamov88@gmail.com>
2025-09-13 21:00:36 +0200

    changed view by task mocks

--- app/components/pages/main/index.tsx
+++ app/components/pages/main/index.tsx
@@ -1,12 +1,14 @@
 
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { useRounds } from "@/lib/hooks/useRounds";
 import { useAuth } from "@/lib/providers/auth-provider";
 import type { Round, RoundUser } from "@/lib/types";
 import { Badge } from '@ui/badge';
 import { Check } from 'lucide-react';
-import { Link, useNavigate } from "react-router";
+import { Link, Navigate, useNavigate } from "react-router";
 
 export function Main() {
   const { data: rounds } = useRounds();
@@ -27,39 +29,32 @@ export function Main() {
           : <Button variant='success' disabled={!(user?.role === 'admin')}> Create </Button>
         }
       </div>
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[300px]">id</TableHead>
-            <TableHead className="w-[100px] ">Status</TableHead>
-            <TableHead className="w-[180px] text-center">Start</TableHead>
-            <TableHead className="w-[180px] text-center hidden md:table-cell">End</TableHead>
-            <TableHead>Configurations</TableHead>
-            <TableHead>Users</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {rounds?.map((round: Round) => (
-            <TableRow key={round.id} onClick={() => handleClick(round.id)} className="cursor-pointer">
-              <TableCell className="font-medium">{round.id}</TableCell>
-              <TableCell>
-                <Check color={round.isActive ? 'green' : 'grey'} />
-              </TableCell>
-              <TableCell >{new Date(round.startAt).toLocaleString()}</TableCell>
-              <TableCell >{new Date(round.endAt).toLocaleString()}</TableCell>
-              <TableCell className="flex flex-col" >
-                <span>Duration: {round.duration}</span>
-                <span>Cooldown: {round.cooldown}</span>
-              </TableCell>
-              <TableCell >
-                {round.users.map((roundUser: RoundUser) => (
-                  <Badge key={roundUser.id} variant='outline'>{roundUser.name}</Badge>
-                ))}
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+      {rounds?.map((round: Round) => (
+        <Card key={round.id} className="max-w-[550px]">
+          <CardHeader>
+              <CardTitle>
+                <Link to={`/round/${round.id}`}>
+                  ● Round ID: {round.id}
+                </Link>
+                
+              </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="flex flex-col">
+              <span>
+                Start: {new Date(round.startAt).toLocaleString()}
+                </span>
+                <span>
+                  End: {new Date(round.endAt).toLocaleString()}
+                </span>
+            </div>
+            <Separator className="my-5" />
+            <div>
+              Статус: {round.isActive ?( round.isInCoolDown ? "Cooldown" : 'Активен') : 'Не Активен'}
+            </div>
+          </CardContent>
+        </Card>
+      ))}
     </>
   )
 }
