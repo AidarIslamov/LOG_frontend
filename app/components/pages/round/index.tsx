030606f86dd3c4d015734c42d6ef7005b39e9e7e
aidar <aidar.islamov88@gmail.com>
2025-09-14 12:12:09 +0200

    changes

--- app/components/pages/round/index.tsx
+++ app/components/pages/round/index.tsx
@@ -1,25 +1,20 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
-import { enterRound, useRound } from "@/lib/hooks/useRound"
+import { Separator } from "@/components/ui/separator";
+import { useRound } from "@/lib/hooks/useRound"
+import type { Round } from "@/lib/types";
 import { useEffect, useRef } from "react";
 import { Navigate } from "react-router"
 
 export function Round({ uid }: { uid: string }) {
-    const { data: round , isLoading } = useRound({ uid });
-    const hasEntered = useRef(false);
+    const { data, isLoading } = useRound({ uid });
+    const {round, message} = data ?? {};
 
-    useEffect(() => {
-        if (!isLoading && round && !hasEntered.current) {
-            hasEntered.current = true;
-            enterRound(uid);
-        }
-    }, [round, isLoading, uid]);
-
-    if(isLoading) {
-        return 'Loading'
+    if (isLoading) {
+        return 'Loading..'
     }
 
-    if(!round) {
-        <Navigate to="/" replace />
+    if (!round) {
+        return <Navigate to="/" replace />
     }
 
     return (
@@ -27,16 +22,61 @@ export function Round({ uid }: { uid: string }) {
             <Card className="max-w-md px-5">
                 <CardHeader>
                     <CardTitle className="mx-auto">
-                        Раунд: <span className="underline">{round?.id }</span>
+                        Раунд: <span className="underline">{round.id}</span>
                     </CardTitle>
                 </CardHeader>
                 <CardContent>
                     <div className="flex justify-center">
                         <img src="/goose.jpg" alt="" />
                     </div>
-                    {/* {JSON.stringify(round)} */}
+                    <div className="my-10">
+                        {round.isActive ?
+                            round.isInCoolDown ? <CooldownStatus round={round} /> : <ActiveStatus round={round} />
+                            : <NotActiveStatus round={round} />
+                        }
+                    </div>
                 </CardContent>
             </Card>
         </div>
     )
-} 
+}
+
+
+function CooldownStatus({ round }: { round: Round }) {
+    return (
+        <div className="flex flex-col justify-center">
+            <span className="mx-auto">Cooldown</span>
+            <span className="mx-auto">до начала раунда 00:15</span>
+        </div>
+    )
+}
+
+function NotActiveStatus({ round }: { round: Round }) {
+    return (
+        <div>
+            <Separator className="w-full my-3" />
+            <div className="flex flex-row justify-between">
+                <span>Всего</span>
+                <span>999999</span>
+            </div>
+            <div className="flex flex-row justify-between">
+                <span>Победитель - Иван</span>
+                <span>100500</span>
+            </div>
+            <div className="flex flex-row justify-between">
+                <span>Мои очки </span>
+                <span>321</span>
+            </div>
+        </div>
+    )
+}
+
+function ActiveStatus({ round }: { round: Round }) {
+    return (
+        <div className="flex flex-col justify-center">
+            <span className="mx-auto">Раунд активен!</span>
+            <span className="mx-auto">До конца осталось: 00:23</span>
+            <span className="mx-auto">Мои очки - 123</span>
+        </div>
+    )
+}
--- app/lib/hooks/useRound.ts
+++ app/lib/hooks/useRound.ts
@@ -6,7 +6,7 @@ function roundQueryOptions({ uid }: { uid: string }) {
     return queryOptions({
         queryKey: ['round', uid],
         queryFn: async () => {
-            const { data }: { data: Round } = await api.get(`/round/${uid}`);
+            const { data }: { data: {round?: Round, message?: string} } = await api.get(`/round/${uid}`); 
             return data;
         },
         refetchOnWindowFocus: true
@@ -23,9 +23,7 @@ export async function createRound(data: RoundFormData) {
     return await api.post('/round', data)
 }
 
-export async function enterRound(uid: string) {
-    return await api.post(`/round/${uid}/enter`);
-}
+
 
 // export async function sendRoundVote(uid: string) {
 //     return await api.post(`/round/${uid}/vote`);
